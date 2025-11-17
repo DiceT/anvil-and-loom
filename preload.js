7d49fe3ab7f6279d52d98f3cfcdcd9f7aea5f8c7
T Guiles <t.guiles@gmail.com>
2025-11-17 18:50:01 -0500

    Blank slate.

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  selectVault: () => ipcRenderer.invoke('select-vault'),
-  listFiles: () => ipcRenderer.invoke('list-files'),
-  readFile: (file) => ipcRenderer.invoke('read-file', file),
-  writeFile: (file, content) => ipcRenderer.invoke('write-file', file, content)
-});
---
+++ readme.md
@@ -0,0 +1,1 @@
+Hi, ChatGPT! Can you see me?
